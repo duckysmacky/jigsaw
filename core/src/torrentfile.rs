a757edd730fdfd094c4bf611428beecdde5404bd
DuckySmacky <kriperoka@gmail.com>
2026-09-05 15:57:56 +0200

    Add 'bencode_get' macro for easier bencode decoding

--- core/src/torrentfile.rs
+++ core/src/torrentfile.rs
@@ -1,6 +1,6 @@
 use std::path::PathBuf;
 
-use jigsaw_bencode::BencodeElement;
+use jigsaw_bencode::{BencodeElement, ByteString as ByteStringType};
 use thiserror::Error;
 
 use crate::bencode::BencodeDict;
@@ -31,45 +31,82 @@ pub struct TorrentFile {
     pub creation_date: Option<u64>,
 }
 
+/// Extract and parse bencode dictionary values with automatic error handling.
+///
+/// This macro eliminates boilerplate when extracting typed values from bencoded dictionaries.
+/// It handles type checking and provides consistent error messages for missing/wrong-type fields.
+///
+/// Four forms:
+/// - `required $key, Type => $parse_fn`: Extract required field and parse with non-fallible function
+/// - `optional $key, Type => $parse_fn`: Extract optional field, returns `Option<T>` with non-fallible parse
+/// - `required $key, Type => errors $parse_fn`: Extract required field with fallible parser returning `Result<T, StructureError>`
+/// - `optional $key, Type => errors $parse_fn`: Extract optional field with fallible parser, returns `Option<T>`
+///
+/// # Example
+/// ```ignore
+/// // Non-fallible parsing (simple value transformation)
+/// let name = bencode_get!(dict: required "name", ByteString => |x| x.to_string())?;
+///
+/// // Fallible parsing (with validation)
+/// let length = bencode_get!(dict: required "length", Int => errors |len: &i64| {
+///     if *len < 0 { return Err(StructureError::NegativeLength) }
+///     Ok(*len as u64)
+/// })?;
+/// ```
+macro_rules! bencode_get {
+    ($dict:tt : required $key:expr, $element_type:ident => $parse_fn:expr) => {{
+        use BencodeElement::*;
+
+        match $dict.get(&$key.into()) {
+            Some($element_type(val)) => Ok($parse_fn(val)),
+            Some(_) => Err(StructureError::WrongType($key.to_string())),
+            None => Err(StructureError::RequiredKeyMissing($key.to_string())),
+        }
+    }};
+    ($dict:tt : optional $key:expr, $element_type:ident => $parse_fn:expr) => {{
+        use BencodeElement::*;
+
+        match $dict.get(&$key.into()) {
+            Some($element_type(val)) => Ok(Some($parse_fn(val))),
+            Some(_) => Err(StructureError::WrongType($key.to_string())),
+            None => Ok(None),
+        }
+    }};
+    ($dict:tt : required $key:expr, $element_type:ident => errors $parse_fn:expr) => {{
+        use BencodeElement::*;
+
+        match $dict.get(&$key.into()) {
+            Some($element_type(val)) => $parse_fn(val),
+            Some(_) => Err(StructureError::WrongType($key.to_string())),
+            None => Err(StructureError::RequiredKeyMissing($key.to_string())),
+        }
+    }};
+    ($dict:tt : optional $key:expr, $element_type:ident => errors $parse_fn:expr) => {{
+        use BencodeElement::*;
+
+        match $dict.get(&$key.into()) {
+            Some($element_type(val)) => $parse_fn(val).map(Some),
+            Some(_) => Err(StructureError::WrongType($key.to_string())),
+            None => Ok(None),
+        }
+    }};
+}
+
 impl TorrentFile {
     pub fn from_bencoded(dict: BencodeDict) -> Result<Self, StructureError> {
-        let announce = match dict.get(&"announce".into()) {
-            Some(BencodeElement::ByteString(announce)) => announce.to_string(),
-            Some(_) => return Err(StructureError::WrongType("announce".to_string())),
-            None => return Err(StructureError::RequiredKeyMissing("announce".to_string())),
-        };
+        let announce = bencode_get!(dict: required "announce", ByteString => |x: &ByteStringType| x.to_string())?;
+        let info = bencode_get!(dict: required "info", Dict => errors |x: &BencodeDict| Info::from_bencoded(x))?;
+        let comment = bencode_get!(dict: optional "comment", ByteString => |x: &ByteStringType| x.to_string())?;
+        let created_by = bencode_get!(dict: optional "created by", ByteString => |x: &ByteStringType| x.to_string())?;
+        let creation_date = bencode_get!(dict: optional "creation date", Int => |x: &i64| *x as u64)?;
 
-        let info = match dict.get(&"info".into()) {
-            Some(BencodeElement::Dict(info_dict)) => Info::from_bencoded(info_dict)?,
-            Some(_) => return Err(StructureError::WrongType("info".to_string())),
-            None => return Err(StructureError::RequiredKeyMissing("info".to_string()))
-        };
-
-        let comment = match dict.get(&"comment".into()) {
-            Some(BencodeElement::ByteString(comment)) => Some(comment.to_string()),
-            Some(_) => return Err(StructureError::WrongType("comment".to_string())),
-            None => None,
-        };
-
-        let created_by = match dict.get(&"created by".into()) {
-            Some(BencodeElement::ByteString(created_by)) => Some(created_by.to_string()),
-            Some(_) => return Err(StructureError::WrongType("created by".to_string())),
-            None => None,
-        };
-
-        let creation_date = match dict.get(&"creation date".into()) {
-            Some(BencodeElement::Int(creation_date)) => Some(*creation_date as u64),
-            Some(_) => return Err(StructureError::WrongType("creation date".to_string())),
-            None => None,
-        };
-
-        return Ok(Self {
+        Ok(Self {
             announce,
             info,
             comment,
             created_by,
             creation_date
-        });
+        })
     }
 }
 
@@ -88,11 +125,7 @@ impl Info {
             return Err(StructureError::RequiredKeyMissing("files/length".to_string()));
         }
 
-        let name = match info_dict.get(&"name".into()) {
-            Some(BencodeElement::ByteString(name)) => name.to_string(),
-            Some(_) => return Err(StructureError::WrongType("name".to_string())),
-            None => return Err(StructureError::RequiredKeyMissing("name".to_string())),
-        };
+        let name = bencode_get!(info_dict: required "name", ByteString => |x: &ByteStringType| x.to_string())?;
 
         let file = if info_dict.contains_key(&"files".into()) {
             let files = match &info_dict[&"files".into()] {
@@ -101,11 +134,7 @@ impl Info {
                     for entry_elem in files_list {
                         match entry_elem {
                             BencodeElement::Dict(entry_dict) => {
-                                let length = match entry_dict.get(&"length".into()) {
-                                    Some(BencodeElement::Int(len)) => *len as u64,
-                                    Some(_) => return Err(StructureError::WrongType("length".to_string())),
-                                    None => return Err(StructureError::RequiredKeyMissing("length".to_string())),
-                                };
+                                let length = bencode_get!(entry_dict: required "length", Int => |len: &i64| *len as u64)?;
 
                                 let path = match entry_dict.get(&"path".into()) {
                                     Some(BencodeElement::List(path_list)) => {
@@ -134,47 +163,36 @@ impl Info {
 
             FileMode::MultipleFiles { files }
         } else {
-            let length = match info_dict[&"length".into()] {
-                BencodeElement::Int(len) => {
-                    if len < 0 { return Err(StructureError::NegativeLength) }
-                    len as u64
-                },
-                _ => return Err(StructureError::WrongType("length".to_string()))
-            };
+            let length = bencode_get!(info_dict: required "length", Int => errors |len: &i64| {
+                if *len < 0 { return Err(StructureError::NegativeLength) }
+                Ok(*len as u64)
+            })?;
 
             FileMode::SingleFile { length }
         };
 
-        let piece_length = match info_dict.get(&"piece length".into()) {
-            Some(BencodeElement::Int(piece_len)) => {
-                if *piece_len < 0 { return Err(StructureError::NegativeLength) }
-                *piece_len as u32
+        let piece_length = bencode_get!(info_dict: required "piece length", Int => errors |len: &i64| {
+            if *len < 0 { return Err(StructureError::NegativeLength) }
+            Ok(*len as u32)
+        })?;
+
+        let pieces_hashes = bencode_get!(info_dict: required "pieces", ByteString => errors |bytes: &ByteStringType| {
+            if bytes.len() % 20 != 0 {
+                return Err(StructureError::PiecesBytesLengthError);
             }
-            Some(_) => return Err(StructureError::WrongType("piece length".to_string())),
-            None => return Err(StructureError::RequiredKeyMissing("piece length".to_string())),
-        };
+            // unwrap will not fail, the chunk size is guaranteed.
+            Ok(bytes.inner()
+                .chunks_exact(20)
+                .map(|ch| ch.try_into().unwrap())
+                .collect::<Vec<[u8; 20]>>())
+        })?;
 
-        let pieces_hashes = match info_dict.get(&"pieces".into()) {
-            Some(BencodeElement::ByteString(bytes)) => {
-                if bytes.len() % 20 != 0 {
-                    return Err(StructureError::PiecesBytesLengthError);
-                }
-                // unwrap will not fail, the chunk size is guaranteed.
-                bytes.inner()
-                    .chunks_exact(20)
-                    .map(|ch| ch.try_into().unwrap())
-                    .collect::<Vec<[u8; 20]>>()
-            },
-            Some(_) => return Err(StructureError::WrongType("pieces".to_string())),
-            None => return Err(StructureError::RequiredKeyMissing("pieces".to_string())),
-        };
-
-        return Ok(Self {
+        Ok(Self {
             name,
             file,
             piece_length,
             pieces_hashes
-        });
+        })
     }
 }
 
